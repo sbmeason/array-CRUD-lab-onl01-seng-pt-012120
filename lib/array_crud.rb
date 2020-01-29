08f77d865fa06e39b3bc533740f95506920803be
Samantha Meason <sbmeason@gmail.com>
2020-01-29 21:53:27 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,29 @@
-def create_an_empty_array
-  
+ def create_an_empty_array
+  []
 end
 
 def create_an_array
-  
+  ["blue","green","red","yellow"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+  add_element_to_end_of_array.push("arrays!")
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array = ["I", "am", "really", "learning"]
+  add_element_to_start_of_array.unshift("wow")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array = ["I", "am", "really", "learning", "arrays!"]
+  remove_element_from_end_of_array.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  remove_element_from_start_of_array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index = ["wow", "I", "am", "really", "learning", "arrays!"]
+  retrieve_element_from_index [2]
 end
 
 def retrieve_first_element_from_array(array)
